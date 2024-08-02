434a3ca1bcb4e2be6fae88a6361a832dd3d9cd09
erfanchangizi86 <erfanchangizi401@gmail.com>
2024-08-02 15:58:16 +0330

    testing

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'products.middlewares.test'
 ]
 
 ROOT_URLCONF = 'djangoProject1.urls'
