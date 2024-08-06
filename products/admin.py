f912a737b4f066f4f23ab8b106072c5c176a0156
erfanchangizi86 <erfanchangizi401@gmail.com>
2024-08-06 10:35:18 +0330

    testing

--- products/admin.py
+++ products/admin.py
@@ -2,13 +2,23 @@ from django.contrib import admin
 
 from products.models import Product,category
 
+@admin.action(description='صفر کردن قیمت محصول')
+def price_to_zero(modeladmin, request, queryset):
+    for a in queryset:
+        a.price = 0
+        a.save()
+
 
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'image')
+    list_filter = ('name', 'price',)
+    search_fields =  ('name', 'price')
+    actions = [price_to_zero]
 
 @admin.register(category)
 class categoryAdmin(admin.ModelAdmin):
     list_display = ('title','url_name')
     list_filter = ('title','url_name')
+    search_fields = ('title','url_name')
